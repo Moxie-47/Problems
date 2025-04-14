67c8a01603176174c10e3dba4575082ed7fa4595
Moxie-47 <prashants9700@gmail.com>
2025-04-14 11:50:14 +0530

    some cool ways to solve this problem

--- 2005-A Simple Palindrom/code.cpp
+++ 2005-A Simple Palindrom/code.cpp
@@ -2,28 +2,82 @@
 #define int long long
 using namespace std;
 
-void solve(){
-    int n = 0 ;
-    cin>>n ;
-    string vowels = "aeiou" ;
-    string ans = "" ;
-    int mul_letters_cnt = n%5 ;
-    int common_cnt = n/5 ;
-    int cnt = 0 ;
-    for(int i = 0 ;i<5 && cnt < n; i++){
-        int temp_cnt = common_cnt ;
-        while(temp_cnt>0){
-            ans+=vowels[i] ;
-            temp_cnt-- ;
-            cnt++ ;
-        }
-        if(mul_letters_cnt){
-            ans+=vowels[i] ;
-            mul_letters_cnt-- ;
-            cnt++ ;
-        }
+// ---- MY way ----
+// void solve(){
+//     int n = 0 ;
+//     cin>>n ;
+//     string vowels = "aeiou" ;
+//     string ans = "" ;
+//     int mul_letters_cnt = n%5 ;
+//     int common_cnt = n/5 ;
+//     int cnt = 0 ;
+//     for(int i = 0 ;i<5 && cnt < n; i++){
+//         int temp_cnt = common_cnt ;
+//         while(temp_cnt>0){
+//             ans+=vowels[i] ;
+//             temp_cnt-- ;
+//             cnt++ ;
+//         }
+//         if(mul_letters_cnt){
+//             ans+=vowels[i] ;
+//             mul_letters_cnt-- ;
+//             cnt++ ;
+//         }
+//     }
+//     cout<<ans<<endl;
+// }
+
+
+// --- other cool way to solve this problem ---
+// -- way 1 ---
+// void solve(){
+//     int n ; 
+//     cin >> n ; 
+ 
+//     string a = "";
+//     string e = "";
+//     string i = "";
+//     string o = "";
+//     string u = "";
+ 
+//     for(int k=0 ; k<n ; k++)
+//     {
+//       if(k%5==0)
+//       a += 'a' ;
+ 
+//       if(k%5==1)
+//       e += 'e' ;
+ 
+//       if(k%5==2)
+//       i += 'i' ;
+ 
+//       if(k%5==3)
+//       o += 'o' ;
+ 
+//       if(k%5==4)
+//       u += 'u' ;
+//     }
+ 
+//     string ans = a+e+i+o+u ;
+ 
+//     cout << ans << endl ;
+// }
+
+// ---- way 2 (this one is special) ----
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    string s = "aeiou";
+    string ans;
+    for (int i = 0; i < 5; i++) {
+        int x = n / (5 - i);
+        ans += string(x, s[i]);
+        n -= x;
     }
-    cout<<ans<<endl;
+    
+    cout << ans << "\n";
 }
 
 int32_t main(){
